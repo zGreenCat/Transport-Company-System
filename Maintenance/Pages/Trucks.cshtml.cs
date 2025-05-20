33b25810ca571fef7d6ef5331e0f4b632a5057b9
zGreenCat <vicente.araya9821@gmail.com>
2025-05-19 21:12:21 -0400

    feat(maintenance): add update truck status

--- Maintenance/Pages/Trucks.cshtml.cs
+++ Maintenance/Pages/Trucks.cshtml.cs
@@ -6,12 +6,32 @@ namespace Maintenance.Pages
     public class TrucksModel : PageModel
     {
         public List<Truck> Trucks;
+
+        [BindProperty]
+        public int TruckId { get; set; }
+
+        [BindProperty]
+        public string NewStatus { get; set; }
         public void OnGet()
         {
             TrucksdbContext context = new TrucksdbContext();
 
             Trucks = context.Trucks.OrderBy(x => x.Id).ToList();
 
+        }
+
+
+        public IActionResult OnPostUpdateStatus() 
+        {
+            TrucksdbContext context = new TrucksdbContext();
+
+            var truck = context.Trucks.Find(TruckId);
+            if (truck != null) 
+            {
+                truck.Status = NewStatus;
+                context.SaveChanges();
+            }
+            return RedirectToPage();
         }
     }
 }
